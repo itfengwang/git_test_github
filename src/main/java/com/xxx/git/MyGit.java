02d28a4d632a9e4d7e070b4b7719e0af374dca0d
itfeng <hello@itfeng.cn>
2022-10-01 20:37:06 +0800

    second commit

--- src/main/java/com/xxx/git/MyGit.java
+++ src/main/java/com/xxx/git/MyGit.java
@@ -2,6 +2,6 @@ package com.xxx.git;
 
 public class MyGit {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
