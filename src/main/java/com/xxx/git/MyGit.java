2dc41fd0f522d502981a1a3701b6c5b973500dc1
itfeng <hello@itfeng.cn>
2022-10-01 20:48:57 +0800

    hot-fix commit1

--- src/main/java/com/xxx/git/MyGit.java
+++ src/main/java/com/xxx/git/MyGit.java
@@ -3,5 +3,7 @@ package com.xxx.git;
 public class MyGit {
     public static void main(String[] args) {
         System.out.println("hello git2");
+        System.out.println("hot-fix---");
+
     }
 }
